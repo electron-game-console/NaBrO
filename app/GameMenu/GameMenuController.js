9ef205df98152a78628fd0632fdca19a8a565579
Zackary Lowery <zlowery@xcjs.com>
2017-06-16 15:48:38 -0400

    Refined the main menu stles.

--- app/GameMenu/GameMenuController.js
+++ app/GameMenu/GameMenuController.js
@@ -9,21 +9,35 @@ GameMenuController.prototype = new BaseController();
 function GameMenuController(node) {
 	var self = this;
 	var proto = Object.getPrototypeOf(this);
-	proto.fileName = __filename;
 
+	proto.fileName = __filename;
 	proto.node = node;
 
 	this.bindEvents = bindEvents;
 	this.loadGames = loadGames;
 
 	function bindEvents() {
-		window.addEventListener('load', self.loadGames);
+		window.addEventListener('load', function() {
+			self.loadGames();
+		});
 	}
 
 	function loadGames() {
-		fs.readdir(path.join(__dirname, '/../../games'), function(err, files) {
+		var gameDirectory = path.join(__dirname, '/../../games');
+
+		fs.readdir(gameDirectory, function(err, dirs) {
 			if(!err) {
-				self.model.games = files;
+				self.model.games = [];
+
+				dirs.forEach(function(dir) {
+					var manifest = JSON.parse(fs.readFileSync(path.join(gameDirectory, dir, 'manifest.json'), 'utf8'));
+
+					self.model.games.push({
+						title: manifest.title,
+						link: dir,
+						imagePath: path.join(gameDirectory, dir, 'logo.png')
+					});
+				});
 
 				var gameList = self.render();
 				self.append(gameList);
